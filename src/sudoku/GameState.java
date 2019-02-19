b2ff339670f3665823fa870fa7666d8a4adec5e8
David Aylaian <davidaylaian@protonmail.com>
2019-02-19 12:50:12 -0500

    Added setters and getters

--- src/sudoku/GameState.java
+++ src/sudoku/GameState.java
@@ -16,6 +16,32 @@ public class GameState
 		emphasis = new boolean[9][9];
 	}
 	
+	public boolean[][] getEmphasis()
+	{
+		return emphasis;
+	}
+	
+	public void setEmphasis(boolean[][] new_emphasis)
+	{
+		emphasis = new_emphasis;
+	}
+	
+	public void setBoards(InternalBoard new_solution, Board new_gameboard)
+	{
+		solution = new_solution;
+		gameboard = new_gameboard;
+	}
+	
+	public InternalBoard getSolution()
+	{
+		return solution;
+	}
+	
+	public Board getGameboard()
+	{
+		return gameboard;
+	}
+	
 	public void save(File f) throws IOException
 	{
 		// stub
