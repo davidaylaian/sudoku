7e29479b425e6b57054655a10be5ca0866333976
David Aylaian <davidaylaian@protonmail.com>
2019-03-04 13:09:29 -0500

    Fixed puzzle classes

--- src/sudoku/Puzzle.java
+++ src/sudoku/Puzzle.java
@@ -1,19 +1,8 @@
-
-package sudoku;
-
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Puzzle {
-	public static void printBoard(int[][] board) {
-		for(int r=0;r<9;r++) {
-			for(int c=0;c<9;c++) {
-				System.out.print(board[r][c]);
-			}
-			System.out.println();
-		}
-	}
 	public static boolean safePlace(int[][] board, int row, int col, int num) {
 		for(int c=0;c<9;c++) {
 			if(board[row][c] == num)
@@ -66,7 +55,7 @@ public class Puzzle {
 		}
 		return false;
 	}
-	public static boolean allSolutions(int[][] board, int[] count) {
+	private static boolean allSolutions(int[][] board, int[] count) {
 		boolean full = true;
 		int row = 0;
 		int col = 0;
@@ -122,71 +111,15 @@ public class Puzzle {
 		}
 		return board;
 	}
-	public static int[][] copyArray(int[][] array) {
+	private static int[][] copyArray(int[][] array) {
 		int[][] copy = new int[array.length][];
 		for(int i=0;i<array.length;i++) {
 			copy[i] = array[i].clone();
 		}
 		return copy;
 	}
-	public static void testSolver() {
-		int[][] board = {
-				{0,0,0,2,6,0,7,0,1},
-				{6,8,0,0,7,0,0,9,0},
-				{1,9,0,0,0,4,5,0,0},
-				{8,2,0,1,0,0,0,4,0},
-				{0,0,4,6,0,2,9,0,0},
-				{0,5,0,0,0,3,0,2,8},
-				{0,0,9,3,0,0,0,7,4},
-				{0,4,0,0,5,0,0,3,6},
-				{7,0,3,0,1,8,0,0,0}
-		};
-		if(solveBoard(board)) {
-			printBoard(board);
-		}
-	}
-	public static void testAllSolutions() {
-		int[][] board = {
-				{2,9,5,7,4,3,8,6,1},
-				{4,3,1,8,6,5,9,0,0},
-				{8,7,6,1,9,2,5,4,3},
-				{3,8,7,4,5,9,2,1,6},
-				{6,1,2,3,8,7,4,9,5},
-				{5,4,9,2,1,6,7,3,8},
-				{7,6,3,5,3,4,1,8,9},
-				{9,2,8,6,7,1,3,5,4},
-				{1,5,4,9,3,8,6,0,0}
-		};
-		int[] count = new int[1];
-		allSolutions(board, count);
-		System.out.println(count[0]);
-		int[][] board2 = {
-				{0,0,0,0,0,0,0,0,0},
-				{0,5,2,0,0,0,6,3,0},
-				{0,4,6,8,0,3,1,5,0},
-				{0,0,7,4,0,9,8,0,0},
-				{0,0,0,0,1,0,0,0,0},
-				{0,0,9,7,0,6,2,0,0},
-				{0,2,8,5,0,4,9,6,0},
-				{0,1,3,0,0,0,7,8,0},
-				{0,0,0,0,0,0,0,0,0}
-		};
-		count = new int[1];
-		allSolutions(board2, count);
-		System.out.println(count[0]);
-	}
-	public static void testGenerator() {
-		int[][] puzzle = createPuzzle();
-		printBoard(puzzle);
-		int[] count = new int[1];
-		allSolutions(puzzle, count);
-		System.out.println(count[0]);
-	}
-	public static void main(String[] str) {
-		//testSolver();
-		//testAllSolutions();
-		testGenerator();
-	}
-
 }
+
+
+
 
