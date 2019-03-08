b2bba848c97ab18d0c9b0df161e623d46b91aed8
Ajit-Mehrotra <33383900+Ajit-Mehrotra@users.noreply.github.com>
2019-03-08 12:29:43 -0500

    Kenneth patch (#9)
    * menu class
    
    * Menu and Window classes as of  now
    
    * window class
    
    * window and menu class as of now

--- src/sudoku/Window.java
+++ src/sudoku/Window.java
@@ -1,9 +1,19 @@
 package sudoku;
-
-public class Window
-{
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class Window{
+	JFrame frame;
+	JPanel contentPane;
 	public Window()
 	{
-		// stub
+		frame = new JFrame("Sudoku");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane = new JPanel();
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		contentPane.add(new Menu());
+		frame.setContentPane(contentPane);
+		frame.setSize(800, 800);
+		frame.setVisible(true);
 	}
 }
