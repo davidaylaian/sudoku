6d56322b43d557ca4ab1c184ea2990679dc6cfe0
David Aylaian <davidaylaian@protonmail.com>
2019-03-13 12:52:56 -0400

    Removed duplicate GameState.undo_enabled

--- src/sudoku/GameState.java
+++ src/sudoku/GameState.java
@@ -48,7 +48,7 @@ public class GameState
 
 		gameBoard[indexX][indexY] = newCell;
   }
-  
+
 	public Cell getCell(int indexX, int indexY)
 	{
 		return gameBoard[indexX][indexY];
@@ -76,11 +76,6 @@ public class GameState
 		Change c = history.get(historyIndex);
 		gameBoard[c.indexX][c.indexY] = c.modifiedCell;
 		historyIndex++;
-	}
-
-	public boolean undo_enabled()
-	{
-		return historyIndex != 0;
 	}
 
 	public boolean undo_enabled()
