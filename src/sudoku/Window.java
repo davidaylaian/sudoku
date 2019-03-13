f33e6462e184bf10e492d6e86bd0b00afdc2aa65
Ajit-Mehrotra <33383900+Ajit-Mehrotra@users.noreply.github.com>
2019-03-13 13:12:29 -0400

    mode in window (#29)

--- src/sudoku/Window.java
+++ src/sudoku/Window.java
@@ -7,15 +7,25 @@ public class Window{
 	JFrame frame;
 	JPanel contentPane;
 	private static GameState game;
+	public final int ENTRY = 0;
+	public final int SOLVING = 1;
+	public int mode;
 	
 	public static GameState getGameState() {
 		return game;
 	}
-
+	public int getMode() {
+		return mode;
+	}
+	public void changeMode() {
+		if(mode == ENTRY)
+			mode = SOLVING;
+		else
+			mode = ENTRY;
+	}
 	public static void setGameState(GameState g) {
 		game = g;
 	}
-
 	
 	public Window()
 	{
