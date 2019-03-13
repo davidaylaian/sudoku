5d4ada1561ae83446b3d46122e4a376f5a0fb0ae
Ajit-Mehrotra <33383900+Ajit-Mehrotra@users.noreply.github.com>
2019-03-13 13:07:46 -0400

    Popup finished (#27)

--- src/sudoku/PopUp.java
+++ src/sudoku/PopUp.java
@@ -1,38 +1,162 @@
 package sudoku;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class PopUp{
+public class PopUp implements ActionListener{
 	
 	JFrame frame;
 	JPanel contentPane;
-	JButton rollDie;
+	JButton upValButton;
+	JButton downValButton;
+	JLabel valueDisplay;
+	JLabel epic;
+	JButton OK, Cancel;
 	private int answer = 0;
 	boolean yesnt;
+	private int hints = 0;
 	
+	// enter "" for noAnswer to not have a no option
 	PopUp(String query, String yesAnswer, String noAnswer) {
+		String[] options;
+		if(!noAnswer.equals("")) {
+			options = new String[2];
+			options[0] = yesAnswer;
+			options[1] = noAnswer;
+			answer = JOptionPane.showOptionDialog(
+					frame,
+					query,
+					"Please Answer Before Proceeding",
+					JOptionPane.YES_NO_OPTION,
+					JOptionPane.QUESTION_MESSAGE,
+					null,
+					options,
+					options[0]
+			);
+		}else {
+			options = new String[1];
+			options[0] = yesAnswer;
+			answer = JOptionPane.showOptionDialog(
+					frame,
+					query,
+					"Please Answer Before Proceeding",
+					JOptionPane.YES_NO_OPTION,
+					JOptionPane.WARNING_MESSAGE,
+					null,
+					options,
+					options[0]
+			);
+		}
 		
-		JOptionPane x = new JOptionPane();
-		String[] options = {yesAnswer,noAnswer};
-		answer = x.showOptionDialog(frame,query,"Please Answer Before Proceeding",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[0]);
 	
+		
+		
 		if(answer == JOptionPane.YES_OPTION) {
 			yesnt = true;
 			
 		}else {
 			yesnt = false;
 		}
-	
+		
+	}
+
+	//use this constructor for getting hints wanted
+	PopUp(String query) {
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		frame = new JFrame("Answer Before Proceeding");
+		
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		/* * Create a content pane with a BoxLayout and empty borders*/
+		contentPane = new JPanel();
+		contentPane.setBorder(BorderFactory.createEmptyBorder(10, 100, 10, 100));
+		contentPane.setBackground(Color.white);
+		contentPane.setLayout(new GridLayout(3, 2, 10, 5));
+
+		/* Create a query Label */
+		epic = new JLabel(query);
+		contentPane.add(epic);
+		
+		/*Create a Value Display Label */
+		valueDisplay = new JLabel("0");
+		contentPane.add(valueDisplay);
+		
+
+		/*Create a down Val button */
+		downValButton = new JButton("-1");
+		downValButton.setActionCommand("-1");
+		
+		downValButton.setAlignmentX(JButton.RIGHT_ALIGNMENT);
+		downValButton.addActionListener(this);
+		downValButton.setBackground(Color.WHITE);
+		contentPane.add(downValButton);
+		
+		/* Create a up Val button */
+		upValButton = new JButton("+1");
+		upValButton.setActionCommand("+1");
+		upValButton.setAlignmentX(JButton.LEFT_ALIGNMENT);
+		upValButton.addActionListener(this);
+		upValButton.setBackground(Color.WHITE);
+		contentPane.add(upValButton);
+		
+		/* Create an OK button */
+		OK = new JButton("OK");
+		OK.addActionListener(this);
+		OK.setActionCommand("OK");
+		contentPane.add(OK);
+		
+		/*Create a cancel button */ 
+		Cancel = new JButton("Cancel");
+		Cancel.addActionListener(this);
+		Cancel.setActionCommand("cancel");
+		contentPane.add(Cancel);
+		
+		/* Add content pane to frame */
+		frame.setContentPane(contentPane);
+		
+		/* Size and then display the frame. */
+		frame.pack();
+		frame.setLocation((int) ((screen.width/2) - (frame.size().getWidth()/2)), (int) ((screen.height/2) - (frame.size().getHeight()/2)));
+		frame.setVisible(true);
 	}
 	
-	public boolean getAnswer() {
+	public boolean getYesOrNo() {
 		return yesnt;
+	}
+	
+	public int getHints() {
+		return hints;
+	}
+	
+	public void actionPerformed(ActionEvent event) { 
+		String y = event.getActionCommand();
+		if(y.equals("+1")) {
+			hints += 1;
+			valueDisplay.setText(Integer.toString(hints));
+		}else if(y.equals("-1")){
+			if(hints > 0) {
+			hints -= 1;
+			valueDisplay.setText(Integer.toString(hints));
+			}
+		}else if(y.equals("cancel")){
+			hints = -1;
+			frame.setVisible(false);
+		}else {
+			frame.setVisible(false);
+		}
+		
+		
 	}
 
 }
-	
-	
-	
