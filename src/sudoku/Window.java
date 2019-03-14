d95549e8f1e0bc178100102d13d9016dbc97d6e9
David Aylaian <davidaylaian@protonmail.com>
2019-03-14 12:35:43 -0400

    Centered Window

--- src/sudoku/Window.java
+++ src/sudoku/Window.java
@@ -1,4 +1,7 @@
 package sudoku;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -11,29 +14,29 @@ public class Window {
 	public static final int ENTRY = 0;
 	public static final int SOLVING = 1;
 	public static int mode;
-	
+
 	public static GameState getGameState() {
 		return game;
 	}
-  
+
 	public static void setGameState(GameState g) {
 		game = g;
 	}
-  
+
 	public static int getMode() {
 		return mode;
 	}
-  
+
 	public static void entryMode() {
 		if(getMode() != ENTRY)
 			mode = ENTRY;
 	}
-  
+
 	public static void solvingMode() {
 		if(getMode() != SOLVING)
 			mode = SOLVING;
 	}
-  
+
 	public Window()
 	{
 		frame = new JFrame("Sudoku");
@@ -42,9 +45,14 @@ public class Window {
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 		contentPane.add(new Menu());
 		frame.setContentPane(contentPane);
-		frame.setSize(630, 800);
-		frame.setLocationRelativeTo(null);
+
 		frame.setVisible(true);
+		frame.pack();
+
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation((int) ((screen.width/2) - (frame.size().getWidth()/2)), (int) ((screen.height/2) - (frame.size().getHeight()/2)));
+		frame.setLocationRelativeTo(null);
+
 		entryMode();
 		setGameState(new GameState(null));
 	}
