155bbd1e8b8f5271d1280568827449f40f36d67c
David Aylaian <davidaylaian@protonmail.com>
2019-03-25 12:51:09 -0400

    Fix of Cell.setState

--- src/sudoku/Cell.java
+++ src/sudoku/Cell.java
@@ -50,10 +50,11 @@ public class Cell {
 	}
 
 	private void setState() {
-		for(int i = 0; i < 3;) {
-			for(int j = 0; j < 3;) {
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
 				if(emphasis[i][j]) {
-					state = ++i + ++j * 3;
+					state = j + i * 3 + 1;
+
 				}
 			}
 		}
